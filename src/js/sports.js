1e9e4fd68f6736df0c0a2897ab70cbc34d8ada63
Md Rasheduzzaman Rashed <rased.cse.2016@gmail.com>
2022-08-30 18:17:53 +0600

    detail button

--- src/js/sports.js
+++ src/js/sports.js
@@ -3,7 +3,7 @@ const topNews = () => {
   fetch(url)
     .then((res) => res.json())
     .then((data) => topGameDisplay(data.sports))
-    .catch((error) => console.log("File missing"));
+    .catch((error) => console.log(error));
 };
 
 const topGameDisplay = (data) => {
@@ -11,18 +11,51 @@ const topGameDisplay = (data) => {
   data.forEach((sport) => {
     const gameDiv = document.createElement("div");
     gameDiv.classList.add("border-2");
-    gameDiv.innerHTML = `
-        <img class="w-full" src="${sport.strSportThumb}" alt="Image Unavilable">
-        <div class="text-center mt-2 font-bold">
-        <h3 class=" text-3xl pb-3">${sport.strSport}</h3>
-        <button onClick="gameDetail('${sport.strSportDescription}')" class="block w-full py-2 text-white bg-blue-700" >More Detail</button>
-        </div> 
-    `;
+
+    const gameImg = document.createElement("img");
+    gameImg.onerror = function () {
+      this.src = "../img/sports.jpg";
+    };
+    gameImg.src = sport.strSportThumb;
+    gameImg.classList.add("w-full");
+    gameDiv.appendChild(gameImg);
+
+    const title = document.createElement("h3");
+    title.innerText = sport.strSport;
+    title.classList.add("text-center", "mt-2", "font-bold", "text-3xl", "pb-3")
+    gameDiv.appendChild(title);
+
+    const gameBtn = document.createElement("button");
+    gameBtn.innerText = "More Detail";
+    gameBtn.classList.add(
+      "block",
+      "w-full",
+      "py-2",
+      "text-white",
+      "bg-blue-700"
+    );
+    gameBtn.addEventListener("click", function () {
+      gameDetail(sport);
+    });
+    gameDiv.appendChild(gameBtn);
     gameField.appendChild(gameDiv);
   });
 };
-const gameDetail = (gameData) => {
-    console.log(gameData);
-};
+const gameDetail = (data) => {
+  const gameTitle = document.getElementById("game-title");
+  const gameDetail = document.getElementById("game-detail-field");
+  const gameIcon = document.getElementById("game-icon");
+  const hideDetail = document.getElementById("game-detail2");
+  hideDetail.classList.replace("hidden", "relative");
+  gameTitle.innerText = data.strSport;
+  gameDetail.innerText = data.strSportDescription;
 
+  gameIcon.innerHTML = `
+  <img src="${data.strSportIconGreen}" alt="">
+  `;
+};
+const hiddenFunction = () => {
+  const hideDetail = document.getElementById("game-detail2");
+  hideDetail.classList.replace("relative", "hidden");
+};
 topNews();
